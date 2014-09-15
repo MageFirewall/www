6df813d18a382da2e3f76b568be6b0015e2c5512
ilampirai <ilam.arkantos@gmail.com>
2014-09-15 23:21:19 +0530

    Fork me link and topline font size

--- index.php
+++ index.php
@@ -81,7 +81,7 @@
 <div class="header_bg" id="home">
 	<div class="container">
 		<div class="head-para">
-			<div class="topline">Opensource Magento Firewall for securing your e-commerce system.</div>
+			<div class="topline"><h4>Opensource Magento Firewall for securing your e-commerce system.</h4></div>
 			<img src="images/magefirewall.png" class="magelogo">
 			<h1>Mage Firewall</h1>
 		</div>
@@ -332,5 +332,6 @@
 				</div>
 		
 		<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<a href="https://github.com/paimpozhil/MageFirewall"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"></a>
 </body>
 </html>
